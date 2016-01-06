9498856da9fc2b662344bbb5309e080b17d3ee6c
JohnFlabs <john_198708@mail.ru>
2016-01-06 17:41:27 +0600

    .

---
+++ rule/InfocityServices/Resources/scripts/page/glossary/GetFlatParts.groovy
@@ -0,0 +1,41 @@
+package page.glossary
+
+import kz.lof.webservices.clients.ump.HumansSearchServiceProxy
+import kz.nextbase.script._Session
+import kz.nextbase.script._Tag
+import kz.nextbase.script._WebFormData
+import kz.nextbase.script._XMLDocument
+import kz.nextbase.script.events._DoScript
+
+class GetFlatParts extends _DoScript {
+	
+	@Override
+	public void doProcess(_Session session, _WebFormData formData,	String lang) {
+		HumansSearchServiceProxy proxy = new HumansSearchServiceProxy(session.user); 
+		
+		try {
+			String streetid = formData.get("streetid")[0];
+			String housenumber = formData.get("house")[0];
+			String flatnumber = formData.get("flat")[0];
+			String flatpart = formData.get("flatnumber")[0];
+			def tag = new _Tag("root","")
+			def result = proxy.getFlatParts(streetid.toInteger(), housenumber, flatnumber)
+			def flats =  new _Tag("flats","");
+			result.each{
+				def entry = new _Tag("entry","");
+				def number = new _Tag("number",it);
+				entry.addTag(number);
+				flats.addTag(entry);
+			}
+			tag.addTag(flats);
+			def xml = new _XMLDocument(tag)
+			setContent(xml);
+		} catch (Exception re) {
+			println(re);
+		}
+		
+	}
+
+	
+
+}
